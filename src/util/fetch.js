59a55c0868423b909b57680f7616a18160ef8faf
Anuj Chopra <anujchopra100@gmail.com>
2023-04-26 13:14:50 +0530

    updated node_modules

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,46 +1,46 @@
-export async function getData(url="" , )
-{
+// export async function getData(url="" , )
+// {
     
 
-    const response = await fetch(`http://localhost:8080${url}` , {
-        method :"GET",
-        mode:"cors",
-        cache:"no-cache",
-        credentials:"same-origin",
-        headers:{
-            "Content-Type":"application/json", 
-            Authorization: "Bearer " +sessionStorage.getItem("access-token"),
-        }, 
-        redirect:"follow",
-        referrerPolicy: "no-referrer",
+//     const response = await fetch(`http://localhost:8080${url}` , {
+//         method :"GET",
+//         mode:"cors",
+//         cache:"no-cache",
+//         credentials:"same-origin",
+//         headers:{
+//             "Content-Type":"application/json", 
+//             Authorization: "Bearer " +sessionStorage.getItem("access-token"),
+//         }, 
+//         redirect:"follow",
+//         referrerPolicy: "no-referrer",
 
         
 
-    })
+//     })
 
-    return response;
+//     return response;
    
    
-}
+// }
 
 
 
-export async function postData(url="" , data={}, header={}) {
+// export async function postData(url="" , data={}, header={}) {
 
-    const response = await fetch(url ,{
-        method :"POST",
-        mode:"cors",
-        cache:"no-cache",
-        credentials:"same-origin",
-        headers:{
-            "Content-Type":"application/json", 
-            Authorization: "Bearer " +sessionStorage.getItem("access-token"),
-            ...header,
-        }, 
-        redirect:"follow",
-        referrerPolicy: "no-referrer",
-        body:JSON.stringify(data),
-    })
-    return response
+//     const response = await fetch(url ,{
+//         method :"POST",
+//         mode:"cors",
+//         cache:"no-cache",
+//         credentials:"same-origin",
+//         headers:{
+//             "Content-Type":"application/json", 
+//             Authorization: "Bearer " +sessionStorage.getItem("access-token"),
+//             ...header,
+//         }, 
+//         redirect:"follow",
+//         referrerPolicy: "no-referrer",
+//         body:JSON.stringify(data),
+//     })
+//     return response
     
-}
+// }
